b5c638a3f83bda822084b669a4b063555cd9e07e
Nate Ferreira <nferreira1729@gmail.com>
2022-09-24 10:19:01 -0400

    Switch to using clap

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,62 @@
-use std::{env, io};
-use io::Write;
+use clap::{Arg, Command};
 
-mod pour_over;
-use pour_over::PourOver;
+fn cli() -> Command<'static> {
+    Command::new("cafe")
+        .about("A coffee brewing helper!")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_invalid_utf8_for_external_subcommands(false)
+        .subcommand(
+            Command::new("brew")
+            .about("Brew constructor")
+            .arg(Arg::new("pourover")
+                .long("pour-over")
+                .short('p'))
+            .arg(Arg::new("coffee-mass")
+                 .long("coffee-mass")
+                 .value_parser(clap::value_parser!(u32))
+                 .short('c')
+                 .required(true)
+                 .takes_value(true)
+                 .value_name("COFFEE_MASS"))
+        )
+}
 
+//fn push_args() -> Vec<clap::Arg<'static>> {
+//    vec![arg!(-m --message <MESSAGE>).required(false)]
+//}
 
-enum BrewType {
-    PourOver
+fn generate_pour_over_instructions(coffee_mass: u32) {
+    // coffee to water ratio.
+    let ratio = 16;
+    let total_brew_mass = coffee_mass * 16;
+    println!("Coffee amount: {}g", coffee_mass);
+    println!("Water required: {}g", coffee_mass * ratio);
+
+    println!("\n\n=== Blooming ===");
+    println!("Add {}g-{}g of bloom water (0-45s)", coffee_mass*2, coffee_mass*3);
+
+    println!("\n=== Main Pour ===");
+    println!("Add up to {} in 30 seconds (45s - 1m15s)", total_brew_mass*3/5);
+
+    println!("\n=== Final Pour ===");
+    println!("Add up to {} over next 30 seconds (1m15s - 1m45s)", total_brew_mass);
+    println!("Stir clockwise and use spoon to knock grinds from sid-wall");
+
 }
 
 fn main() {
-    let cli_args: Vec<String> = env::args().collect();
-    if cli_args.len() < 2 {
-        println!("Hey you didn't pass me an argument! I don't know what to do!"); 
-    } else {
-    
-        let command = &cli_args[1];
+    let matches = cli().get_matches();
 
-        if command == "brew" {
-            println!("Starting brewing process!");
-            configure_brew();
-        }
+    match matches.subcommand() {
+        Some(("brew", sub_matches)) => {
+
+            // Getting the coffee mass
+            let coffee_mass: u32  = *sub_matches.get_one("coffee-mass")
+                .expect("Error reading coffee mass.");
+            
+            generate_pour_over_instructions(coffee_mass);
+        },
+        _ => (),
     }
-}
-
-fn configure_brew() {
-    let brew_type = get_brew_type().expect("Error getting brew Type");
-
-    // TODO Make grind_size configureable
-    let grind_size = 14;
-
-    match brew_type {
-        BrewType::PourOver => PourOver::configure_brew(grind_size),
-    };
-}
-
-fn get_brew_type() -> io::Result<BrewType> { 
-    let brew_type = loop {
-        let mut buffer = String::new();
-        print!("\nPlease select brewing process\n");
-        print!("(1) Pour Over (Chemx, V60, etc.)");
-        io::stdout().flush()?;
-        io::stdin().read_line(&mut buffer)?;
-
-        // Attempt to convert input to number
-        let num: u32 = match buffer.trim().parse() {
-            Ok(n) => n,
-            Err(_) => continue,
-        };
-
-        if num == 1 {
-            break BrewType::PourOver;
-        }
-    };  
-    Ok(brew_type)
 }
